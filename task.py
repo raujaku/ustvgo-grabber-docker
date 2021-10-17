820b4249f96fff053bd5d50d9455f7b81e8fb63a
colino17 <51090962+colino17@users.noreply.github.com>
2021-10-17 15:19:50 +0000

    Update task.py

--- task.py
+++ task.py
@@ -1,28 +1,155 @@
-import ssl
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from datetime import datetime
+banner = r'''
+                    #########################################################################
+                    #      ____            _           _   __  __                           #
+                    #     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
+                    #     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
+                    #     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
+                    #     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
+                    #                   |__/                                                #
+                    #                                  >> https://github.com/benmoose39     #
+                    #########################################################################
+'''
+print(banner)
 
-smtp_port = "<SMTP_SERVER_PORT>"
-smtp_server = "<SMTP_SERVER_ADRESS>"
-sender_mail = "<EMAIL>"
-sender_pw = "<PASSWORD>"
-recipients = ["<EMAIL_1>", "<EMAIL_2>"]
+import os
+import sys
 
-def send_alert(title, message):
-    message = MIMEMultipart("alternative")
-    message["Subject"] = f"{title}"
-    message["From"] = sender_mail
-    message["To"] = ", ".join(recipients)
-    body = f"{message}"
-    message.attach(MIMEText(body, "html"))
-    context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls(context=context)
-        server.login(sender_mail, sender_pw)
-        server.sendmail(sender_mail, recipients, message.as_string())
+windows = False
+python = 'python3'
+if 'win' in sys.platform:
+    windows = True
+    python = 'python'
 
-if __name__ == "__main__":
-    print(f"Running script at {datetime.now()}")
-    send_alert("This message is sent every 30 minutes", "<h1>Hello World from a Docker container</h1>")
+def done():
+    sys.exit()
+    
+print('[*] Checking dependencies...')
+while True:
+    try:
+        import requests
+        from tqdm import tqdm
+        break
+    except ModuleNotFoundError as e:
+        module = str(e)[17:-1]
+        print(f'[*] Installing {module} module for python')
+        #os.system(f'{python} -m pip insbanner = r'''
+                    #########################################################################
+                    #      ____            _           _   __  __                           #
+                    #     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
+                    #     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
+                    #     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
+                    #     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
+                    #                   |__/                                                #
+                    #                                  >> https://github.com/benmoose39     #
+                    #########################################################################
+'''
+print(banner)
+
+import os
+import sys
+
+windows = False
+python = 'python3'
+if 'win' in sys.platform:
+    windows = True
+    python = 'python'
+
+def done():
+    sys.exit()
+    
+print('[*] Checking dependencies...')
+while True:
+    try:
+        import requests
+        from tqdm import tqdm
+        break
+    except ModuleNotFoundError as e:
+        module = str(e)[17:-1]
+        print(f'[*] Installing {module} module for python')
+        #os.system(f'{python} -m pip install --upgrade pip')
+        try:
+            if os.system(f'{python} -m pip install {module}') != 0:
+                raise error
+        except Exception:
+            print(f'[!] Error installing "{module}" module. Do you have pip installed?')
+            input(f'[!] Playlist generation failed. Press Ctrl+C to exit...')
+            done()
+
+def grab(name, code, logo):
+    data = {'stream': code}
+    m3u = s.post('https://ustvgo.tv/data.php', data=data).text
+    playlist.write(f'\n#EXTINF:-1 tvg-id="{code}" group-title="ustvgo" tvg-logo="{logo}", {name}')
+    playlist.write(f'\n{m3u}')
+
+total = 0
+with open('../ustvgo_channel_info.txt') as file:
+    for line in file:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        total += 1
+
+s = requests.Session()
+with open('../ustvgo_channel_info.txt') as file:
+    with open('../ustvgo.m3u', 'w') as playlist:
+        print('[*] Generating your playlist, please wait...\n')
+        playlist.write('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Theitfixer85/myepg/master/blueepg.xml.gz"')
+        playlist.write(f'\n{banner}\n')
+        pbar = tqdm(total=total)
+        for line in file:
+            line = line.strip()
+            if not line or line.startswith('~~'):
+                continue
+            line = line.split('|')
+            name = line[0].strip()
+            code = line[1].strip()
+            logo = line[2].strip()
+            pbar.update(1)
+            grab(name, code, logo)
+        pbar.close()
+        print('\n[SUCCESS] Playlist is generated!\n')
+        done()
+        tall --upgrade pip')
+        try:
+            if os.system(f'{python} -m pip install {module}') != 0:
+                raise error
+        except Exception:
+            print(f'[!] Error installing "{module}" module. Do you have pip installed?')
+            input(f'[!] Playlist generation failed. Press Ctrl+C to exit...')
+            done()
+
+def grab(name, code, logo):
+    data = {'stream': code}
+    m3u = s.post('https://ustvgo.tv/data.php', data=data).text
+    playlist.write(f'\n#EXTINF:-1 tvg-id="{code}" group-title="ustvgo" tvg-logo="{logo}", {name}')
+    playlist.write(f'\n{m3u}')
+
+total = 0
+with open('../ustvgo_channel_info.txt') as file:
+    for line in file:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        total += 1
+
+s = requests.Session()
+with open('../ustvgo_channel_info.txt') as file:
+    with open('../ustvgo.m3u', 'w') as playlist:
+        print('[*] Generating your playlist, please wait...\n')
+        playlist.write('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Theitfixer85/myepg/master/blueepg.xml.gz"')
+        playlist.write(f'\n{banner}\n')
+        pbar = tqdm(total=total)
+        for line in file:
+            line = line.strip()
+            if not line or line.startswith('~~'):
+                continue
+            line = line.split('|')
+            name = line[0].strip()
+            code = line[1].strip()
+            logo = line[2].strip()
+            pbar.update(1)
+            grab(name, code, logo)
+        pbar.close()
+        print('\n[SUCCESS] Playlist is generated!\n')
+        done()
+        
