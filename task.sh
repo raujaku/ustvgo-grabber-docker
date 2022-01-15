a9eca322f09c0cdd394dfd7556beca4082314c1b
raujaku <47949202+raujaku@users.noreply.github.com>
2022-01-15 13:36:44 -0600

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
 python3 /task.py
-sleep 5
-
-curl -X POST -d '{"cmd":"update.m3u"}' http://$xTeveIP:$xTevePORT/api/
-sleep 5
-
-curl -X POST -d '{"cmd":"update.xmltv"}' http://$xTeveIP:$xTevePORT/api/
-sleep 5
-
-curl -X POST -d '{"cmd":"update.xepg"}' http://$xTeveIP:$xTevePORT/api/
-sleep 5
